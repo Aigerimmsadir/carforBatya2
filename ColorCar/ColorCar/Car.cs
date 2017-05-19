d0c270d2714599359c1cd60606bc3c994afdd707
Aigerimmsadir <aigerimsadir@gmail.com>
2017-05-20 01:37:12 +0600

    Add files via upload

---
+++ ColorCar/ColorCar/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorCar
+{
+   public class Car
+    {
+
+       public GraphicsPath gp = new GraphicsPath();
+      public void Draw(int x, int y)
+        {
+            Rectangle r = new Rectangle(x, y, 30, 20);
+            Rectangle r2 = new Rectangle(x + 30, y + 10, 10, 10);
+            Rectangle r3 = new Rectangle(x + 4, y + 19, 10, 10);
+            Rectangle r4 = new Rectangle(x + 16, y + 19, 10, 10);
+            gp.AddRectangle(r);
+            gp.AddRectangle(r2);
+            gp.AddEllipse(r3);
+            gp.AddEllipse(r4);
+        }
+        
+    }
+}
---
+++ ColorCar/ColorCar/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorCar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+     
+            InitializeComponent();
+      
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.Black;
+            
+        }
+
+   
+        Pen pen1 = new Pen(Color.Red);
+        Pen pen2 = new Pen(Color.Blue);
+
+      static int x = 10, y = 110;
+        Car car = new Car();
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        if(color==Colors.Blue)    e.Graphics.FillPath(pen2.Brush, car.gp);
+        else e.Graphics.FillPath(pen1.Brush, car.gp);
+
+        }
+enum Colors
+        {
+            Blue,Red
+        }
+        Colors color = Colors.Blue;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (color == Colors.Blue)
+            {
+                x = x + 5;
+                car.gp.Reset();
+                car.Draw(x, y);
+            }
+            if (x == 520) color = Colors.Red;
+            Refresh();
+        }
+    }
+}
